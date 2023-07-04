317b97c34107e8845d3e10f330868588716aae94
keipy27 <kevinandris27@gmail.com>
2023-07-04 14:24:52 +1200

    handling eventsadded

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import List from './components/List';
+import AddToList from './components/AddToList';
 
-interface IState {
+export interface IState {
   people: {
     name: string
     age: number
@@ -29,6 +30,7 @@ function App() {
     <div className="App">
       <h1>People Invited to my Party</h1>
       <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
